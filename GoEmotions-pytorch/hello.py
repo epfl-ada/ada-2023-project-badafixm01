091f240041fd1f7d7f05fd6bd4765284c5a7e401
Zhang-XC <zhang_xiaocheng@outlook.com>
2023-12-21 16:32:48 +0100

    Merge branch Berta

--- GoEmotions-pytorch/hello.py
+++
@@ -1,7 +0,0 @@
-with open(R"C:\Users\Berta\Desktop\EPFL\ADA\PROJECT\ada-2023-project-badafixm01\GoEmotions-pytorch\final\chunk_mapping.txt", 'r', encoding='utf-8') as file:
-    file_content = file.read()
-    print('hi')
-# Normalize line breaks, then split
-normalized_content = file_content.replace('\r\n', '\n').replace('\r', '\n')
-paragraphs = [paragraph for paragraph in normalized_content.split('\n\n') if paragraph]
-print('hi')
